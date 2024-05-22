d1e5ca0dfae8b68ab892c9418cb4500d639416aa
Al Tsaplin <al.tsaplin@gmail.com>
2024-05-22 23:25:39 +0300

    add(homework) For lesson #04

--- script.js
+++ script.js
@@ -1,98 +1,26 @@
 'use strict';
-/* 
-1). Переменная lang может принимать 2 значения: 'ru' 'en'.
-Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. Решите задачу
-	a) через if,
-	b) через switch-case
-	c) через многомерный массив без if'ов и switch’ей.
+/*
+1) Создайте функцию, которая принимает 1 аргумент (название произвольное)
+	— Если в качестве аргумента передана не строка - функция оповещает об этом пользователя
+	— В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце
+	— Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
 */
-const htmlTag = document.querySelector('html');
-const languageSwitchBtn = htmlTag.querySelector('.language-switcher');
 
-let currentLanguage = htmlTag.getAttribute('lang');
+const text = 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Свой осталось парадигматическая, маленькая обеспечивает, переписали залетают если рекламных толку наш моей запятой его лучше, несколько от всех назад подпоясал повстречался? Большого.';
 
-const switchLanguage = function () {
-	currentLanguage === 'ru' ? htmlTag.setAttribute('lang', 'en')
-		: htmlTag.setAttribute('lang', 'ru');
-
-	currentLanguage = htmlTag.getAttribute('lang');
-
-	let langsArray = [];
-	langsArray.ru = [
-		'Понедельник\n',
-		'\tВторник\n',
-		'\t\tСреда\n',
-		'\t\t\tЧетверг\n',
-		'\t\t\t\tПятница\n',
-		'\t\t\t\t\tСуббота\n',
-		'\t\t\t\t\t\tВоскресенье'];
-	langsArray.en = ['Monday\n',
-		'\tTuesday\n',
-		'\t\tWednesday\n',
-		'\t\t\tThursday\n',
-		'\t\t\t\tFriday\n',
-		'\t\t\t\t\tSaturday\n',
-		'\t\t\t\t\t\tSunday'];
-
-	console.clear();
-	console.log(langsArray[currentLanguage].join(''));
-	/* switch (currentLanguage) {
-		case 'ru':
-			console.log(
-				'Понедельник\n' +
-				'\tВторник\n' +
-				'\t\tСреда\n' +
-				'\t\t\tЧетверг\n' +
-				'\t\t\t\tПятница\n' +
-				'\t\t\t\t\tСуббота\n' +
-				'\t\t\t\t\t\tВоскресенье');
-			break;
-		case 'en':
-			console.log(
-				'Monday\n' +
-				'\tTuesday\n' +
-				'\t\tWednesday\n' +
-				'\t\t\tThursday\n' +
-				'\t\t\t\tFriday\n' +
-				'\t\t\t\t\tSaturday\n' +
-				'\t\t\t\t\t\tSunday'); 
-	}
-	*/
-	/* 
-	if (currentLanguage === 'ru') {
-		console.log(
-			'Понедельник\n' + 
-			'\tВторник\n' + 
-			'\t\tСреда\n' + 
-			'\t\t\tЧетверг\n' + 
-			'\t\t\t\tПятница\n' + 
-			'\t\t\t\t\tСуббота\n' + 
-			'\t\t\t\t\t\tВоскресенье');
+const addEllipsesForText = function (arg) {
+	if (typeof arg !== 'string') {
+		console.log('Введена не строка!');
+		return;
 	} else {
-		console.log(
-			'Monday\n' + 
-			'\tTuesday\n' + 
-			'\t\tWednesday\n' + 
-			'\t\t\tThursday\n' + 
-			'\t\t\t\tFriday\n' + 
-			'\t\t\t\t\tSaturday\n' + 
-			'\t\t\t\t\t\tSunday');
-	} 
-*/
+		arg = arg.trim();
+
+		if (arg.length > 29) {
+			arg = arg.slice(0, 30) + '...';
+		}
+
+		return arg;
+	}
 }
 
-languageSwitchBtn.addEventListener('click', switchLanguage);
-
-// * ======================== * \\
-
-/* 2). У нас есть переменная namePerson. Если значение этой переменной “Артём” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
-	Решить задачу с помощью нескольких тернарных операторов, без использования if или switch  */
-
-let namePerson;
-
-namePerson = 'Александр';
-
-namePerson === 'Артём' ? console.log('директор') :
-	namePerson === 'Александр'
-		? console.log('преподаватель') :
-		console.log('студент');
+console.log(addEllipsesForText(text));
