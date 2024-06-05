a8bafc441bcee1d7b0ab74db285770c05844a8f9
Al Tsaplin <al.tsaplin@gmail.com>
2024-06-05 07:28:19 +0300

    wip(homework) For lesson #06 (still counter with recoursion has to be done)

--- script.js
+++ script.js
@@ -19,123 +19,43 @@
 Загаданное число и оставшиеся кол-во попыток должно храниться «в замыкании»
 */
 
-const ROLLBACK = 5;
+function start() {
+	let counter = 0;
+	if (counter < 3) {
+		function ask() {
+			const n = 33;
+			const num = prompt('Угадайте число от 1 до 100');
+			const isNan = function (num) {
+				return !isNaN(parseFloat(num) && isFinite(num));
+			}
 
-let title, 
-		screens, 
-		screenPrice, 
-		adaptive, 
-		allServicePrices, 
-		fullPrice, 
-		servicePercentPrice, 
-		service1, 
-		service2;
-
-// * ==================== Усложнённое задание №1 ==================== * \\
-const isNumber = function (num) {
-	return !isNaN(parseFloat(num) && isFinite(num));
-}
-
-const ask = function () {
-	title = prompt('Как называется ваш проект?', ' КаЛьКулятор Верстки');
-	screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-
-	do {
-		screenPrice = +prompt('Сколько будет стоить данная работа? (₽)').trim();
-	} while (!isNumber(screenPrice));
-
-	adaptive = confirm('Нужен ли адаптив на сайте?');
-}
-
-const getAllServicePrices = function () {
-	let servicePrice;
-	let sum = 0;
-
-	for (let index = 0; index < 2; index++) {
-		if (index === 0) {
-			service1 = prompt('Какой дополнительный тип услуги нужен?');
-		} else if (index === 1) {
-			service2 = prompt('Какой дополнительный тип услуги нужен?');
+			switch (true) {
+				case (+num < 1 || +num > 100):
+					alert('Загаданное число должно быть в диапазоне между 1 и 100 включительно. Попробуйте снова!');
+					return counter++;
+				case (+num === n):
+					alert('Поздравляю, Вы угадали!!!\nОбновите страницу, чтобы сыграть еще раз. (F5 на клавиатуре)');
+					return counter = 10;
+				case +num > n:
+					alert('Загаданное число меньше предложенного вами числа. Попробуйте снова!');
+					return counter++;
+				case +num < n:
+					alert('Загаданное число больше предложенного вами числа. Попробуйте снова!');
+					return counter++;
+				case num === null:
+					alert('Игра окончена.\nОбновите страницу, чтобы сыграть еще раз. (F5 на клавиатуре)');
+					return counter = 10;
+				case isNan():
+					alert('Упс, кажется вы ввели вовсе не цифру! Попробуйте снова!');
+					break;
+			}
+			return counter;
 		}
-
-		do {
-			servicePrice = prompt('Сколько будет стоить данная работа? (₽)').trim();
-		} while (!isNumber(servicePrice));
-
-		sum += +servicePrice;
-	}
-
-	return sum;
-}
-
-const showTypeOf = function (variable) {
-	console.log(variable, typeof variable);
-}
-
-const getFullPrice = function () {
-	return screenPrice + allServicePrices;
-}
-
-const getServicePercentPrice = function () {
-	return fullPrice - (fullPrice * (ROLLBACK / 100));
-}
-
-const getTitle = function () {
-	if (!title) {
-		return console.log('Название проекта не было указано!');
+		ask();
+		return counter;
 	} else {
-		return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase();
+		alert('Ваше количество попыток на угадывание загаданного числа закончилось, вы проиграли. ¯\\_(ツ)_/¯')
 	}
 }
 
-const getRollbackMessage = function (price) {
-		switch (true) {
-			case price >= 30000:
-				console.log(`Поздравляем! Вы получили от нас скидку 10%! Ваша цена теперь 	составляет ${price - (price * 0.1)}₽!`);
-				break;
-			case price <= 30000 && price >= 15000:
-				console.log(`Поздравляем! Вы получили от нас скидку 5%! Ваша цена теперь 	составляет ${price - (price * 0.05)}₽!`);
-				break;
-			case price <= 15000 && price >= 0:
-				console.log(`Скидка не предусмотрена. Ваша цена ${price}₽.`);
-				break;
-			case price < 0:
-				console.log('Что-то пошло не так...');
-				break;
-	}
-}
-
-// screensVariety = screens.toLowerCase().split(', ');
-/* const getRollbackMessage = function (price) {
-	if (price >= 30000) {
-		return "Даем скидку в 10%";
-	} else if (price >= 15000 && price < 30000) {
-		return "Даем скидку в 5%";
-	} else if (price >= 0 && price < 15000) {
-		return "Скидка не предусмотрена";
-	} else {
-		return "Что-то пошло не так";
-	}
-} */
-
-ask();
-
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice();
-servicePercentPrice = getServicePercentPrice();
-title = getTitle();
-
-showTypeOf(title);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
-
-console.log('allServicePrices:', allServicePrices);
-
-console.log(getRollbackMessage(fullPrice));
-console.log(screens);
-console.log(servicePercentPrice);
-
-console.log(
-	'Стоимость верстки экранов ' + screenPrice + ' рублей. \n' +
-	'Стоимость разработки сайта ' + fullPrice + ' рублей.'
-);
+start();
