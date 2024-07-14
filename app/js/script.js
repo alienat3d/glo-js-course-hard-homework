94c70c8bb218ff69b6258458420e5861dc6368ba
Al Zaplin <zaplin.dev@gmail.com>
2024-07-14 05:55:51 +0300

    add(homework) For Lesson #14 If CMS-checkbox checked opens CMS-options block with select and input for "other"-option Recalc price considering those field.

--- app/js/script.js
+++ app/js/script.js
@@ -5,10 +5,11 @@
 
 [✓] 2) При выборе option с значением "Другое" (value=other) должен открываться блок с классом main-controls__input, но только тот, что внутри блока с классом hidden-cms-variants (ВНИМАНИЕ, блоков с классом main-controls__input в проекте много, искать стоит внутри определенного элемента)
 
-[] 3) Если в input[type=checkbox] выбран вариант с числовым value (value=50) то высчитываем общую стоимость работы с учетом данного value. Значение + процент от общей стоимости работы
+[✓] 3) Если в input[type=checkbox] выбран вариант с числовым value (value=50) то высчитываем общую стоимость работы с учетом данного value. Значение + процент от общей стоимости работы
 Пример: общая стоимость работы равна 30.000. При выборе варианта WordPress с value=50 стоимость работы рассчитывается так: 30.000 + 15.000 = 45.000 (15.000 это 50% от 30.000)
+		? CMS должен увеличивать на определенный процент Итоговая стоимость, вот. В Стоимости услуг не отображается, это работа, а не услуга. То есть. Если ранее калькулятор рассчитывал Итоговая стоимость как 30.000 то они должны при выборе 50% пересчитаться как 45.000. Так же пересчитывается и стоимость с учетом отката
 
-[] 4) При нажатии на кнопку Сброс метод reset() должен возвращать в исходное состояние и блок с классом hidden-cms-variants
+[✓] 4) При нажатии на кнопку Сброс метод reset() должен возвращать в исходное состояние и блок с классом hidden-cms-variants
 */
 
 const title = document.getElementsByTagName('h1')[0];
@@ -22,7 +23,8 @@ const optionCheckboxes = document.querySelectorAll('.main-controls__checkbox .cu
 const сmsCheckbox = document.getElementById('cms-open');
 const сmsOptionsBlock = document.querySelector('.hidden-cms-variants');
 const сmsOptionsSelect = сmsOptionsBlock.querySelector('#cms-select');
-const сmsOtherInput = сmsOptionsBlock.querySelector('.main-controls__input');
+const сmsOtherInputBlock = сmsOptionsBlock.querySelector('.main-controls__input');
+const сmsOtherInput = сmsOtherInputBlock.querySelector('input');
 // == / [hw] ==
 const rollbackController = document.querySelector('.rollback input');
 const rollbackControllerValue = document.querySelector('.rollback .range-value');
@@ -53,33 +55,48 @@ const appData = {
 	init() {
 		this.addTitle();
 		startBtn.addEventListener('click', () => {
-			if (this.checkFields(screenSelects, screenInputs)) {
-				startBtn.style.display = 'none';
-				resetBtn.style.display = 'block';
-				this.start();
-				this.disableEnableInputs(true);
+			if (сmsCheckbox.checked) {
+				if (this.checkFields(screenSelects, screenInputs) && this.checkCmsOptionsFields(сmsOptionsSelect, сmsOtherInput)) {
+					this.showElem(resetBtn, 'block');
+					this.hideElem(startBtn);
+					this.start();
+					this.disableEnableInputs(true);
+				}
+			} else if (!сmsCheckbox.checked) {
+				if (this.checkFields(screenSelects, screenInputs)) {
+					this.showElem(resetBtn, 'block');
+					this.hideElem(startBtn);
+					this.start();
+					this.disableEnableInputs(true);
+				}
 			}
 		});
 		resetBtn.addEventListener('click', () => {
-			startBtn.style.display = 'block';
-			resetBtn.style.display = 'none';
+			this.showElem(startBtn, 'block');
+			this.hideElem(resetBtn);
 			this.reset();
 			this.disableEnableInputs(false);
 		});
 		plusBtn.addEventListener('click', () => this.addScreenBlock());
 		rollbackController.addEventListener('input', () => this.operateRangeInput());
 		// == [hw] ==
-		сmsCheckbox.addEventListener('change', () => this.showCmsOptionsBlock());
-		сmsOptionsSelect.addEventListener('change', () => {
-			// if (сmsOptionsSelect.selectedIndex > 0) this.addCmsPrice();
-			if (сmsOptionsSelect.value = 'other') this.showCmsOtherInput();
-		});
+		сmsCheckbox.addEventListener('change', () =>
+			сmsCheckbox.checked ?
+				this.showElem(сmsOptionsBlock) :
+				this.hideElem(сmsOptionsBlock)
+		);
+		сmsOptionsSelect.addEventListener('change', () =>
+			сmsOptionsSelect.value === 'other' ?
+				this.showElem(сmsOtherInputBlock) :
+				this.hideElem(сmsOtherInputBlock)
+		);
 		// == / [hw] ==
 	},
 	start() {
 		this.addScreens();
 		this.addServices();
 		this.addPrices();
+		this.addCmsPrice();
 		this.showResult();
 		// this.logger();
 	},
@@ -101,18 +118,23 @@ const appData = {
 		optionCheckboxes.forEach(checkbox => checkbox.disabled = value);
 		plusBtn.disabled = value;
 		rollbackController.disabled = value;
+		сmsOptionsSelect.disabled = value;
+		сmsOtherInput.disabled = value;
 	},
 	checkFields(selects, inputs) {
 		let error = false;
-
-		inputs.forEach(input => {
-			if (input.value === '' || input.value <= 0 || !this.isNumber(input.value)) error = true;
-		});
-
 		selects.forEach(select => {
 			if (select.selectedIndex === 0) error = true;
 		});
-
+		inputs.forEach(input => {
+			if (input.value === '' || input.value <= 0 || !this.isNumber(input.value)) error = true;
+		});
+		return !error;
+	},
+	checkCmsOptionsFields(select, input) {
+		let error = false;
+		if (select.selectedIndex === 0) error = true;
+		if (select.value === 'other' && (input.value === '' || input.value <= 0 || !this.isNumber(input.value))) error = true;
 		return !error;
 	},
 	operateRangeInput() {
@@ -124,9 +146,18 @@ const appData = {
 	showResult() {
 		total.value = this.screenPrice;
 		totalCountOther.value = this.servicePricesPercent + this.servicePricesNumber;
-		fullTotalCount.value = this.fullPrice;
-		totalCountRollback.value = Math.round(this.servicePercentPrice);
 		totalCount.value = this.screensCount;
+		// == [hw] ==
+		if (сmsCheckbox.checked) {
+			this.addCmsPrice();
+			const totalCmsPrice = this.fullPrice + (this.fullPrice * this.cmsPercentPrice);
+			fullTotalCount.value = Math.round(totalCmsPrice);
+			totalCountRollback.value = Math.round(totalCmsPrice - (totalCmsPrice * (this.rollback / 100)));
+		} else {
+			fullTotalCount.value = Math.round(this.fullPrice);
+			totalCountRollback.value = Math.round(this.servicePercentPrice);
+		}
+		// == / [hw] ==
 	},
 	addScreens() {
 		screens = document.querySelectorAll('.screen');
@@ -201,33 +232,31 @@ const appData = {
 		this.rollback = 0;
 		this.fullPrice = 0;
 		this.servicePercentPrice = 0;
+		this.cmsPercentPrice = 0;
 		this.servicesPercent = {};
 		this.servicesNumber = {};
 	},
 	clearTagValues() {
 		rollbackController.value = 0;
 		rollbackControllerValue.textContent = '0%';
-		screens[0].querySelector('input').value = 0;
+		screens[0].querySelector('input').value = '';
 		screens[0].querySelector('select').selectedIndex = 0;
 		optionCheckboxes.forEach(checkbox => checkbox.checked = false);
+		this.hideElem(сmsOptionsBlock);
+		сmsOptionsSelect.selectedIndex = 0;
+		сmsOtherInput.value = '';
 	},
 	// == [hw] ==
-	showCmsOptionsBlock() {
-		сmsOptionsBlock.style.display = 'flex';
+	showElem(elem, prop = 'flex') {
+		elem.style.display = prop;
 	},
-	showCmsOtherInput() {
-		сmsOtherInput.style.display = 'flex';
+	hideElem(elem) {
+		elem.style.display = 'none';
 	},
 	addCmsPrice() {
-		if (this.fullPrice > 0) {
-			if (сmsOptionsSelect.value = 'other') {
-				this.fullPrice += +сmsOtherInput.value;
-				console.log(appData.fullPrice);
-			} else {
-				this.fullPrice += +сmsOptionsSelect.value;
-				console.log(appData.fullPrice);
-			}
-		}
+		сmsOptionsSelect.value === 'other' ?
+			this.cmsPercentPrice = +сmsOtherInput.value / 100 :
+			this.cmsPercentPrice = +сmsOptionsSelect.value / 100;
 	},
 	// == / [hw] ==
 	logger() {
