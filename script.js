39e94faa95a07f0f7376e2134eba67f78caeadbd
Al Zaplin <zaplin.dev@gmail.com>
2024-07-22 17:42:50 +0300

    feat(RegExp) Finds timestamps and wrap it into <b> tags

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 /*
 [✓] 1.Написать скрипт, который заменяет слово "функция" и его однокоренные слова в div с id=task1 на «<strong>функция</strong>». 
 
-[] 2. Написать скрипт который в div с id=task2 найдет время. Время имеет формат часы:минуты. И часы, и минуты состоят из двух цифр, пример: 09:00.
+[✓] 2. Написать скрипт который в div с id=task2 найдет время. Время имеет формат часы:минуты. И часы, и минуты состоят из двух цифр, пример: 09:00.
 заключить найденное время в тег <b></b>
 
 [] 3. Создать запрос во всем документе найти текст в кавычках и заключить его в теги <mark></mark>
@@ -16,11 +16,18 @@
 */
 
 const taskBlock1 = document.getElementById('task1');
+const taskBlock2 = document.getElementById('task2');
 
 const addTagStrong = () => {
 	taskBlock1.innerHTML = taskBlock1.innerHTML.replace(/функ[а-я]+/gi, str => {
 		return `<strong>${str}</strong>`;
 	});
 }
+const addTagB = () => {
+	taskBlock2.innerHTML = taskBlock2.innerHTML.replace(/\b\d\d:\d\d/g, str => {
+		return `<b>${str}</b>`;
+	});
+}
 
 addTagStrong();
+addTagB();
