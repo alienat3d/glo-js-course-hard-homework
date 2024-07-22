bb51240c93726f93cba43018c78adc8bdea08db1
Al Zaplin <zaplin.dev@gmail.com>
2024-07-22 18:02:16 +0300

    feat(RegExp) Adding <mark> everywhere, where finds "" or «»

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 [✓] 2. Написать скрипт который в div с id=task2 найдет время. Время имеет формат часы:минуты. И часы, и минуты состоят из двух цифр, пример: 09:00.
 заключить найденное время в тег <b></b>
 
-[] 3. Создать запрос во всем документе найти текст в кавычках и заключить его в теги <mark></mark>
+[✓] 3. Создать запрос во всем документе найти текст в кавычках и заключить его в теги <mark></mark>
 
 [] 4. Замените в документе домены вида http://site.ru на <a href="http://site.ru">site.ru</a>, 
 
@@ -17,17 +17,27 @@
 
 const taskBlock1 = document.getElementById('task1');
 const taskBlock2 = document.getElementById('task2');
+const paragraphs = taskBlock2.querySelectorAll('p');
 
-const addTagStrong = () => {
+const addTagStrong = () =>
 	taskBlock1.innerHTML = taskBlock1.innerHTML.replace(/функ[а-я]+/gi, str => {
 		return `<strong>${str}</strong>`;
 	});
-}
-const addTagB = () => {
+const addTagB = () =>
 	taskBlock2.innerHTML = taskBlock2.innerHTML.replace(/\b\d\d:\d\d/g, str => {
 		return `<b>${str}</b>`;
 	});
+const addTagMark = () => {
+	taskBlock1.innerHTML = taskBlock1.innerHTML.replace(/["«][а-я\s—]+["»]/gi, str => {
+		return `<mark>${str}</mark>`;
+	});
+	paragraphs.forEach(p => {
+		p.innerHTML = p.innerHTML.replace(/["«][а-я\s]+[»"]/gi, str => {
+			return `<mark>${str}</mark>`;
+		});
+	});
 }
 
+addTagMark();
 addTagStrong();
 addTagB();
