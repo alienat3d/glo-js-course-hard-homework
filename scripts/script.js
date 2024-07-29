ef4c98ca94eda24a332e46d104e0926417f1289d
Al Zaplin <zaplin.dev@gmail.com>
2024-07-29 16:12:46 +0300

    feat(moving squares) Moving squares func & reset positions by click on button.

--- scripts/script.js
+++ scripts/script.js
@@ -2,44 +2,89 @@
 
 const gameBody = document.querySelector('.square-body');
 let blocks = gameBody.querySelectorAll('.block');
-const blocksArray = [...blocks];
+const resetBtn = document.querySelector('.btn-reset');
 
-const moveElemInArray = (arr, elem, moveToIdx) => {
-	console.log("Original array: " + arr);
-	let moveEle = elem;
-	let moveToIndex = moveToIdx;
-	while (moveEle < 0) {
-		moveEle += arr.length;
-	}
-	while (moveToIndex < 0) {
-		moveToIndex = moveToIndex + arr.length;
-	}
-	if (moveToIndex >= arr.length) {
-		let un = moveToIndex - arr.length + 1;
-		while (un--) {
-				arr.push(undefined);
-		}
-	}
-	arr.splice(moveToIndex, 0, arr.splice(moveEle, 1));
-	console.log("After move: " + arr);
+const initialBlocksArray = [...blocks];
+let blocksArray = [...blocks];
+
+const swap = (arr, a, b) => arr[a] = arr.splice(b, 1, arr[a])[0];
+
+const addClickListeners = () => {
+	blocks.forEach((block, idx) => {
+
+		block.addEventListener('click', (evt) => {
+			const tgt = evt.target;
+
+			if (tgt.closest('.top') && idx > 4) {
+				swap(blocksArray, idx, idx - 5);
+				render();
+			} else if (tgt.closest('.right') &&
+				idx !== 4 &&
+				idx !== 9 &&
+				idx !== 14 &&
+				idx !== 19 &&
+				idx !== 24) {
+				swap(blocksArray, idx, idx + 1);
+				render();
+			} else if (tgt.closest('.bottom') && idx < 20) {
+				swap(blocksArray, idx, idx + 5);
+				render();
+			} else if (tgt.closest('.left') &&
+				idx !== 0 &&
+				idx !== 5 &&
+				idx !== 10 &&
+				idx !== 15 &&
+				idx !== 20) {
+				swap(blocksArray, idx, idx - 1);
+				render();
+			}
+		})
+	});
 }
 
-blocks.forEach((block, idx, arr) => {
-	block.addEventListener('click', (evt) => {
-		const tgt = evt.target;
-		if (tgt.closest('.top')) {
-			console.log('move up');
-		} else if (tgt.closest('.right')) {
-			console.log('move right')
-			// moveElemInArray(blocksArray, blocksArray[idx], idx + 1);
-			// console.log(blocksArray);
-		} else if (tgt.closest('.bottom')) {
-			console.log('move bottom')
-		} else if (tgt.closest('.left')) {
-			console.log('move left')
-			moveElemInArray(blocksArray, block, idx - 1);
-			console.log(blocksArray);
-		}
-	})
+const render = () => {
+	gameBody.innerHTML = '';
+
+	blocksArray.forEach(block => {
+		gameBody.insertAdjacentHTML('beforeend', `
+			<div class="block">
+				<div class="block-number">${block.querySelector('.block-number').textContent}</div>
+				<div class="block-btn">
+					<div class="arrow left"><img src="img/arrow-left.svg" alt=""></div>
+					<div class="arrow right"><img src="img/arrow-right.svg" alt=""></div>
+					<div class="arrow top"><img src="img/arrow-up.svg" alt=""></div>
+					<div class="arrow bottom"><img src="img/arrow-down.svg" alt=""></div>
+				</div>
+			</div>
+		`);
+	});
+
+	blocks = gameBody.querySelectorAll('.block');
+
+	addClickListeners();
+}
+
+resetBtn.addEventListener('click', () => {
+	gameBody.innerHTML = '';
+	initialBlocksArray.forEach((block, idx) => {
+		gameBody.insertAdjacentHTML('beforeend', `
+			<div class="block">
+				<div class="block-number">${idx + 1}</div>
+				<div class="block-btn">
+					<div class="arrow left"><img src="img/arrow-left.svg" alt=""></div>
+					<div class="arrow right"><img src="img/arrow-right.svg" alt=""></div>
+					<div class="arrow top"><img src="img/arrow-up.svg" alt=""></div>
+					<div class="arrow bottom"><img src="img/arrow-down.svg" alt=""></div>
+				</div>
+			</div>
+		`);
+	});
+
+	blocksArray = initialBlocksArray;
+
+	blocks = gameBody.querySelectorAll('.block');
+
+	addClickListeners();
 });
 
+addClickListeners();
--- scripts/test.js
+++ scripts/test.js
@@ -5,38 +5,38 @@ console.log("Original array: " + arr);
 
 // Position where from the element is 
 // going to move here 'Ruby' is moved 
-let moveEle = 3;
+let elemIdx = 3;
 
 // Position at which element is to be moved 
 // here 'Ruby' is moved to  index 1 which is 
 // index of 'Java'
-let moveToIndx = 1;
+let moveToIdx = 1;
 
 // If actual index of moved element is 
 // less than 0 when 'moveEle += array size'
-while (moveEle < 0) {
-	moveEle += arr.length;
+while (elemIdx < 0) {
+	elemIdx += arr.length;
 }
 
 // Where the element to be moved f that 
 // index is less than 0 when 
 // 'moveToIndx += array size'
-while (moveToIndx < 0) {
-	moveToIndx = moveToIndx + arr.length;
+while (moveToIdx < 0) {
+	moveToIdx = moveToIdx + arr.length;
 }
 
 // If 'moveToIndx' is greater than the 
 // size of the array then with need to 
 // push 'undefined' in the array.
-if (moveToIndx >= arr.length) {
-	let un = moveToIndx - arr.length + 1;
+if (moveToIdx >= arr.length) {
+	let un = moveToIdx - arr.length + 1;
 	while (un--) {
 			arr.push(undefined);
 	}
 }
 // Here element of 'moveEle' is removed and 
 // pushed at 'moveToIndx' index
-arr.splice(moveToIndx, 0, arr.splice(moveEle, 1));
+arr.splice(moveToIdx, 0, arr.splice(elemIdx, 1));
 
 // Print the array after moving
 console.log("After move: " + arr);
